afcb3bc548e7f546e09877e1e523ab6cad2cf346
Stephen Kelly <sbk13@my.fsu.edu>
2023-06-09 19:36:03 -0500

    fixed routing issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,7 @@ export default async function RootLayout({
   console.log(header_url);
   if (
     header_url === "http://localhost:3000/" ||
-    header_url === "http://more.website/"
+    header_url === "https://more.website/"
   ) {
     return (
       <html lang="en">
